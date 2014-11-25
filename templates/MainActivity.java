0113eda05f381a3acad0052a142461444eaa3725
fritzvd <f.vandeventer@gmail.com>
2014-11-25 17:38:52 +0100

    remove breaking stuff for java template

--- templates/MainActivity.java
+++ templates/MainActivity.java
@@ -6,41 +6,6 @@ import android.util.Log;
 public class MainActivity extends org.haxe.lime.GameActivity {
     private static final String TAG = "MyActivity";
 
-    @Override
-    protected void onCreate(Bundle state) {
-        super.onCreate(state);
-
-   }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        cb.onStart(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        cb.onStop(this);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        cb.onDestroy(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        if (cb.onBackPressed())
-            return;
-        else
-            super.onBackPressed();
-    }
-
     public static void showLink() {
     }
 }
