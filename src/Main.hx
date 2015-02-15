c2fcfb5e9e350a027be159498735b869f1f6b496
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-02-15 19:30:04 +0100

    calculate and adjust aspect

--- src/Main.hx
+++ src/Main.hx
@@ -7,21 +7,31 @@ class Main extends Engine
     public var plays:Int;
     public var scaling:Float;
     public var music:Sfx;
+    public var ASPECT:Float;
 
 	override public function init()
 	{
-        //HXP.stage.smoothing = false;
 #if debug
 		HXP.console.enable();
 #end        
-        scaling = HXP.windowWidth / 1024;
-		//scaling = 1;
-   /*     this.scaleX = HXP.windowWidth / 1024;*/
-		//this.scaleY = HXP.windowHeight / 640;
-//#if ios
-		//this.scaleX = this.scaleX / 2;
-		//this.scaleY = this.scaleY / 2;
-/*#end*/
+
+        /**
+         * This is the "standard width"
+         * 1024 * 640
+         * The aspect is 16:10 (that's weird huh?)
+         * So we need to calculate what the largest
+         * version of that is on the screen we're displaying
+         *
+         */
+
+        resizeForAspect();  
+        scaling = HXP.width / 1024;
+/*        this.scaleX = HXP.windowWidth / 1024;*/
+        /*this.scaleY = HXP.windowHeight / 640;*/
+#if ios
+        this.scaleX = this.scaleX / 2;
+        this.scaleY = this.scaleY / 2;
+#end
         plays = 0;
         HXP.scene = new scenes.TitleScreen();
 #if !debug
@@ -29,6 +39,19 @@ class Main extends Engine
         music.loop();
 #end
 	}
+
+    public function resizeForAspect () {
+        var aspect = HXP.windowWidth / HXP.windowHeight;
+        if (aspect > ASPECT) {
+            var newHeight = Math.floor(HXP.windowWidth / ASPECT);
+            HXP.resize(HXP.windowWidth, newHeight);
+        } else if (aspect < ASPECT) {
+            var newWidth = Math.floor(HXP.windowHeight * ASPECT);
+            HXP.resize(newWidth, HXP.windowHeight);
+        }
+
+
+    }
 
 	public static function main() {
     
